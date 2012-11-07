81f4f0397433b15dcdbd9da4b0d592f4fe7fa2f2
Matthias Specht <mspecht@esri.com>
2012-11-07 15:39:17 +0000

    working on OutputHandlers / Encoders

--- examples/prt4maya/client/src/wrapper/MayaOutputHandler.h
+++ examples/prt4maya/client/src/wrapper/MayaOutputHandler.h
@@ -50,7 +50,7 @@ public:
 	MayaOutputHandler(const MPlug* plug, MDataBlock* data, MStringArray* shadingGroups, MIntArray* shadingRanges) : mPlug(plug), mData(data), mShadingGroups(shadingGroups), mShadingRanges(shadingRanges) { }
 	virtual ~MayaOutputHandler() { }
 
-	virtual prt::Status cgaError(size_t isIndex, int32_t shapeID, prt::OutputHandler::CGAErrorLevel level, int32_t methodId, int32_t pc, const wchar_t* message) { throw std::runtime_error("Not implemented yet"); }
+	virtual prt::Status cgaError(size_t isIndex, int32_t shapeID, prt::CGAErrorLevel level, int32_t methodId, int32_t pc, const wchar_t* message) { throw std::runtime_error("Not implemented yet"); }
 	virtual prt::Status cgaPrint(size_t isIndex, int32_t shapeID, const wchar_t* txt) { throw std::runtime_error("Not implemented yet"); }
 	virtual prt::Status cgaReportBool(size_t isIndex, int32_t shapeID, const wchar_t* key, bool value) { throw std::runtime_error("Not implemented yet"); }
 	virtual prt::Status cgaReportFloat(size_t isIndex, int32_t shapeID, const wchar_t* key, double value) { throw std::runtime_error("Not implemented yet"); }
