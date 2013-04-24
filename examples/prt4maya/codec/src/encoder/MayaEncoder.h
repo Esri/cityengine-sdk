66ddd4cbe533bbe016914c95890c629d6b2fcd5b
Simon Haegler <shaegler@esri.com>
2013-04-24 15:40:05 +0000

    fixed maya encoder

--- examples/prt4maya/codec/src/encoder/MayaEncoder.h
+++ examples/prt4maya/codec/src/encoder/MayaEncoder.h
@@ -50,7 +50,7 @@ private:
 class MayaEncoderFactory : public prtx::EncoderFactory {
 public:
 	MayaEncoderFactory() {
-		setInfo(getContentType(), getID(), getName(), getDescription());
+		setInfo(getContentType(), getID(), getName(), getDescription(), L"");
 		prt::AttributeMapBuilder* defaultOptions = prt::AttributeMapBuilder::create();
 		setDefaultOptions(defaultOptions->createAttributeMap());
 		defaultOptions->destroy();
