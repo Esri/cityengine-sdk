660a27875501df20d89e87be743f2e639fbd4f1f
Simon Haegler <shaegler@esri.com>
2014-03-04 20:07:59 +0100

    removed cmake file

--- examples/prt4maya/prt.cmake
+++
@@ -1,5 +0,0 @@
-# cmake helper to locate the prt binaries for all examples.
-# Please set it to the cmake subfolder of your CityEngine SDK installation.
-
-set(prt_DIR "/fasthome/shaegler/prt_trunk/com.esri.prt.build/cmake")
-
