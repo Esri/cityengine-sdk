c24f7a1f5a960b1682b3ead33a84552004c55bcf
Simon Haegler <shaegler@esri.com>
2014-03-21 17:45:53 +0100

    msvc compile fix (include prt_stdint.h for int32_t)

--- examples/prt4maya/client/src/prt4maya/Utilities.h
+++ examples/prt4maya/client/src/prt4maya/Utilities.h
@@ -13,12 +13,15 @@
 #include "maya/MStatus.h"
 #include "maya/MFloatPointArray.h"
 
+#include "prt/prt_stdint.h"
+
+
 #define DO_DBG 0
 #define MCHECK(_stat_) {if(MS::kSuccess != _stat_) { prtu::dbg("maya err at %s:%d: %s %d\n", __FILE__, __LINE__, _stat_.errorString().asChar(), _stat_.statusCode());}}
 
 
 namespace prtu {
-	
+
 
 const char* filename(const char* path);
 
