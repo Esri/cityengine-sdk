fc9f2acafc65470c20aaf37090bc6b7bb36a84aa
Simon Haegler <shaegler@esri.com>
2014-04-16 15:42:02 +0200

    switch VC version depending on maya directory

--- examples/prt4maya/build.bat
+++ examples/prt4maya/build.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 
+set MAYA=%1
 set BUILDTYPE=Release
 set GENERATOR="NMake Makefiles"
 
@@ -12,8 +13,14 @@ cmake -G %GENERATOR% -DCMAKE_BUILD_TYPE=%BUILDTYPE% ../src
 nmake install
 popd
 
+set CLIENTVC=0.0
+SET MAYA|FINDSTR /b "MAYA="|FINDSTR /i 2012 >nul
+IF ERRORLEVEL 1 (set CLIENTVC=9.0)
+SET MAYA|FINDSTR /b "MAYA="|FINDSTR /i 2014 >nul
+IF ERRORLEVEL 1 (set CLIENTVC=10.0)
+
 pushd client
-call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio %CLIENTVC%\VC\vcvarsall.bat" amd64
 rd /S /Q build install
 mkdir build
 cd build
