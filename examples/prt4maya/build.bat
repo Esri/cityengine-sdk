f50850ba8bee1aec20f329c4ed857859b5cb6a23
Simon Haegler <shaegler@esri.com>
2014-04-17 16:19:13 +0200

    added readme for prt4maya and improved windows build.bat

--- examples/prt4maya/build.bat
+++ examples/prt4maya/build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-set MAYA=%1
+set maya_DIR=%1
 set BUILDTYPE=Release
 set GENERATOR="NMake Makefiles"
 
@@ -14,8 +14,8 @@ nmake install
 popd
 
 set CLIENTVC=0.0
-if %MAYA:~-4% == "2012" (set CLIENTVC=9.0)
-if %MAYA:~-4% == "2014" (set CLIENTVC=10.0)
+if %maya_DIR:~-4% == "2012" (set CLIENTVC=9.0)
+if %maya_DIR:~-4% == "2014" (set CLIENTVC=10.0)
 
 pushd client
 call "C:\Program Files (x86)\Microsoft Visual Studio %CLIENTVC%\VC\vcvarsall.bat" amd64
