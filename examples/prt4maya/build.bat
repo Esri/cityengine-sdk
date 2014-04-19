77e58fe517dce75d09664eb934a07153dee7b7cc
Simon Haegler <shaegler@esri.com>
2014-04-19 12:52:22 +0200

    fixed quoting

--- examples/prt4maya/build.bat
+++ examples/prt4maya/build.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 
-set maya_DIR=%1
+set maya_DIR=%~1
 set BUILDTYPE=Release
 set GENERATOR="NMake Makefiles"
 
 pushd codec
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
 rd /S /Q build install
 mkdir build
 cd build
@@ -14,11 +14,11 @@ nmake install
 popd
 
 set CLIENTVC=0.0
-if %maya_DIR:~-4% == "2012" (set CLIENTVC=9.0)
-if %maya_DIR:~-4% == "2014" (set CLIENTVC=10.0)
+if "%maya_DIR:~-4%" == "2012" (set CLIENTVC=9.0)
+if "%maya_DIR:~-4%" == "2014" (set CLIENTVC=10.0)
 
 pushd client
-call "C:\Program Files (x86)\Microsoft Visual Studio %CLIENTVC%\VC\vcvarsall.bat" amd64
+call "%ProgramFiles(x86)%\Microsoft Visual Studio %CLIENTVC%\VC\vcvarsall.bat" amd64
 rd /S /Q build install
 mkdir build
 cd build
