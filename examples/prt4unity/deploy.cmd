4678df781267c35f5a77133ea08efd0dc9de5af6
unknown <simo6772@zrh-bonker-win7.esri.com>
2014-04-28 11:35:10 +0200

    make deploy script independent of compile configuration

--- examples/prt4unity/deploy.cmd
+++ examples/prt4unity/deploy.cmd
@@ -1,7 +1,5 @@
 @ECHO ON
 
-pushd examples\prt4unity\install\Release
-
 mkdir MyUnityProject\Assets\Plugins
 mkdir MyUnityProject\Assets\Prt4Unity\Editor
 mkdir MyUnityProject\Assets\Prt4Unity\Scripts
@@ -21,5 +19,3 @@ move com.esri.prt.examples.unity.dll ProgramFiles(x86)\Unity\prt
 move com.esri.prt.codecs.dll ProgramFiles(x86)\Unity\prt
 move com.esri.prt.adaptors.dll ProgramFiles(x86)\Unity\prt
 move VueExport.dll ProgramFiles(x86)\Unity\prt
-
-popd
