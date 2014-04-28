222fa838b0da4a17af7e91d77b317be4903429ea
unknown <simo6772@zrh-bonker-win7.esri.com>
2014-04-28 11:58:35 +0200

    make prt4unity deploy script more flexible

--- examples/prt4unity/deploy.cmd
+++ examples/prt4unity/deploy.cmd
@@ -14,17 +14,16 @@ move prt4unity.dll MyUnityProject\Assets\Plugins\
 copy ..\..\src\scripts\Prt4UnityEditor.cs MyUnityProject\Assets\Prt4Unity\Editor\
 move ..\..\src\scripts\Prt4Unity.cs MyUnityProject\Assets\Prt4Unity\Scripts\
 
-mkdir ProgramFiles(x86)\Unity\Editor
-mkdir ProgramFiles(x86)\Unity\prt
+mkdir ProgramFiles(x86)\Unity\Editor\prt
 
 copy %PRT%\bin\com.esri.prt.core.dll ProgramFiles(x86)\Unity\Editor\
 copy %PRT%\bin\com.esri.prt.loader.dll ProgramFiles(x86)\Unity\Editor\
 copy %PRT%\bin\flexnet_prt.dll ProgramFiles(x86)\Unity\Editor\
 
-move com.esri.prt.examples.unity.dll ProgramFiles(x86)\Unity\prt
-copy %PRT%\lib\com.esri.prt.codecs.dll ProgramFiles(x86)\Unity\prt
-copy %PRT%\lib\com.esri.prt.adaptors.dll ProgramFiles(x86)\Unity\prt
-copy %PRT%\lib\VueExport.dll ProgramFiles(x86)\Unity\prt
+move com.esri.prt.examples.unity.dll ProgramFiles(x86)\Unity\Editor\prt
+copy %PRT%\lib\com.esri.prt.codecs.dll ProgramFiles(x86)\Unity\Editor\prt
+copy %PRT%\lib\com.esri.prt.adaptors.dll ProgramFiles(x86)\Unity\Editor\prt
+copy %PRT%\lib\VueExport.dll ProgramFiles(x86)\Unity\Editor\prt
 
 7z a prt4unity_win32_%PRT_BUILD%_%BT%.zip MyUnityProject\* ProgramFiles(x86)\*
 popd
