5af3089f4194a3e759b437dd92bb3b59b8e61b11
Simon Haegler <shaegler@esri.com>
2014-05-02 18:29:50 +0200

    prt examples: working on synchronized package naming

--- examples/prt4maya/build.sh
+++ examples/prt4maya/build.sh
@@ -2,6 +2,26 @@
 
 BUILDTYPE=Release
 
+if [ $# -eq 0 ]
+then
+	CLIENT_TARGET=install
+	VER_MAJOR=0
+	VER_MINOR=0
+	VER_MICRO=0
+else
+  	CLIENT_TARGET=package
+	VER_MAJOR=$1
+	VER_MINOR=$2
+	VER_MICRO=$3
+fi
+
+if [ -z "$2" ]
+  then
+    PRT4MAYA_VERSION=0
+  else
+  	PRT4MAYA_VERSION=$2
+fi
+
 pushd codec
 rm -rf build install && mkdir build
 pushd build
@@ -14,7 +34,7 @@ pushd client
 # TODO: setup CC/CXX for gcc 4.1.2 to be on the safe side (as required by maya 2012)
 rm -rf build install && mkdir build
 pushd build
-cmake -DCMAKE_BUILD_TYPE=${BUILDTYPE} ../src
-make install
+cmake -DCMAKE_BUILD_TYPE=${BUILDTYPE} -DPRT4MAYA_VERSION_MAJOR=${VER_MAJOR} -DPRT4MAYA_VERSION_MINOR=${VER_MINOR} -DPRT4MAYA_VERSION_MICRO=${VER_MICRO} ../src
+make ${CLIENT_TARGET}
 popd
 popd
