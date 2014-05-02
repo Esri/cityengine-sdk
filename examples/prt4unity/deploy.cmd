4480ffc4bfe472d0f620638014b9ec97651dbb02
Simon Haegler <shaegler@esri.com>
2014-05-02 16:48:07 +0200

    improved prt4unity package naming

--- examples/prt4unity/deploy.cmd
+++ examples/prt4unity/deploy.cmd
@@ -2,7 +2,10 @@
 
 set BT=%~1
 set PRT=%~2
-set PRT_BUILD=%~3
+set PRT_VERSION_MAJOR=%~3
+set PRT_VERSION_MINOR=%~4
+set PRT_VERSION_MICRO=%~5
+set PRT4UNITY_BUILD_ID=%~6
 
 pushd examples\prt4unity\install\%BT%
 
@@ -25,5 +28,5 @@ copy %PRT%\lib\com.esri.prt.codecs.dll ProgramFiles(x86)\Unity\Editor\prt
 copy %PRT%\lib\com.esri.prt.adaptors.dll ProgramFiles(x86)\Unity\Editor\prt
 copy %PRT%\lib\VueExport.dll ProgramFiles(x86)\Unity\Editor\prt
 
-7z a prt4unity_win32_%PRT_BUILD%_%BT%.zip MyUnityProject\* ProgramFiles(x86)\*
+7z a esri_prt4unity_%PRT4UNITY_BUILD_ID%_cesdk_%PRT_VERSION_MAJOR%_%PRT_VERSION_MINOR%_%PRT_VERSION_MICRO%_%BT%_win_x86.zip MyUnityProject\* ProgramFiles(x86)\*
 popd
