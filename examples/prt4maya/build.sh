c1b5527244a03e8838188d08ce42995b9a70a665
Simon Haegler <shaegler@esri.com>
2014-05-05 11:27:47 +0200

    added licensing/readme stuff to linux prt4maya packages

--- examples/prt4maya/build.sh
+++ examples/prt4maya/build.sh
@@ -31,7 +31,7 @@ popd
 popd
 
 pushd client
-# TODO: setup CC/CXX for gcc 4.1.2 to be on the safe side (as required by maya 2012)
+# TODO: enforce CC/CXX for gcc 4.1.2 to be on the safe side (as required by maya 2012)
 rm -rf build install && mkdir build
 pushd build
 cmake -DCMAKE_BUILD_TYPE=${BUILDTYPE} -DPRT4MAYA_VERSION_MAJOR=${VER_MAJOR} -DPRT4MAYA_VERSION_MINOR=${VER_MINOR} -DPRT4MAYA_VERSION_MICRO=${VER_MICRO} ../src
