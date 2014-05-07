4b48ef64f24886de1450dbcf1b9057ecbe9f2fa7
unknown <simo6772@zrh-bonker-win7.esri.com>
2014-05-07 08:01:02 +0200

    prt4unity: packaging fixes

--- examples/prt4unity/deploy.cmd
+++ examples/prt4unity/deploy.cmd
@@ -30,9 +30,9 @@ copy %PRT%\lib\com.esri.prt.adaptors.dll ProgramFiles(x86)\Unity\Editor\prt
 copy %PRT%\lib\VueExport.dll ProgramFiles(x86)\Unity\Editor\prt
 
 copy ..\..\README.windows .
-copy ..\..\..\license license
+copy ..\..\..\..\license license
 
-7z a esri_prt4unity_%PRT4UNITY_BUILD_ID%_cesdk_%PRT_VERSION_MAJOR%_%PRT_VERSION_MINOR%_%PRT_VERSION_MICRO%_%BT%_win_x86.zip MyUnityProject\* ProgramFiles(x86)\*
+7z a esri_prt4unity_%PRT4UNITY_BUILD_ID%_cesdk_%PRT_VERSION_MAJOR%_%PRT_VERSION_MINOR%_%PRT_VERSION_MICRO%_%BT%_win_x86.zip MyUnityProject\* ProgramFiles(x86)\* license\* README.windows
 popd
 
 endlocal
