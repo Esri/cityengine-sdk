7d742ea67fa9ca6ced184f288954fe3d1f2626fb
Simon Haegler <shaegler@esri.com>
2016-05-27 14:44:20 +0200

    prt4maya: fixed package creation

--- examples/prt4maya/build.bat
+++ examples/prt4maya/build.bat
@@ -8,16 +8,17 @@ if "%~1"=="" (
 
 setlocal
 
+set CLIENT_TARGET=install
 set prt_DIR=%~1
 set maya_DIR=%~2
 
 set VER_MAJOR=0
 set VER_MINOR=0
 set VER_MICRO=0
-if not "%~4"=="" (
-	set VER_MAJOR=%~2
-	set VER_MINOR=%~3
-	set VER_MICRO=%~4
+if not "%~3"=="" (
+	set VER_MAJOR=%~3
+	set VER_MINOR=%~4
+	set VER_MICRO=%~5
 	set CLIENT_TARGET=package
 )
 
@@ -32,7 +33,7 @@ call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 mkdir build\codec
 pushd build\codec
 "%CMAKE_EXECUTABLE%" -G %GENERATOR% -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../install ../../src/codec
-nmake install
+nmake all
 popd
 endlocal
 
@@ -41,7 +42,7 @@ call "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
 mkdir build\client
 pushd build\client
 "%CMAKE_EXECUTABLE%" -G %GENERATOR% -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../install -DPRT4MAYA_VERSION_MAJOR=%VER_MAJOR% -DPRT4MAYA_VERSION_MINOR=%VER_MINOR% -DPRT4MAYA_VERSION_MICRO=%VER_MICRO% ../../src/client
-nmake install
+nmake %CLIENT_TARGET%
 popd
 endlocal
 
