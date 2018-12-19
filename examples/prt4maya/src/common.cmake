802b6913cea00f409a51b9f0d247abe0dbc5968f
Simon Haegler <shaegler@esri.com>
2018-12-19 11:21:50 +0100

    CE-6207: cmake updates (mainly support for Visual Studio generator)

--- examples/prt4maya/src/common.cmake
+++ examples/prt4maya/src/common.cmake
@@ -1,6 +1,10 @@
 ###
-### common helpers for both codec and client projects
+### common helpers for both codec and client targets
 ###
+
+if(NOT CMAKE_BUILD_TYPE)
+	set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Choose one of: Debug Release RelWithDebInfo MinSizeRel")
+endif()
 
 
 ### look for the PRT libraries
@@ -12,27 +16,32 @@ endif()
 
 find_package(prt CONFIG REQUIRED)
 set(CESDK_VERSION "cesdk_${PRT_VERSION_MAJOR}_${PRT_VERSION_MINOR}_${PRT_VERSION_MICRO}")
+message(STATUS "Using prt_DIR = ${prt_DIR} with version ${PRT_VERSION_MAJOR}.${PRT_VERSION_MINOR}.${PRT_VERSION_MICRO}")
 
 
 ### autodesk maya installation location
 
 if(NOT maya_DIR)
-	message(FATAL_ERROR "maya_DIR has not been set. please use '-Dmaya_DIR=xxx' to set it to the location of maya (must include devkit).")
+	if(WIN32)
+		set(maya_DIR "C:/Program Files/Autodesk/Maya2018")
+	else()
+		set(maya_DIR "/opt/autodesk/maya2018")
+	endif()
 endif()
-message(STATUS "detected maya_DIR: ${maya_DIR}")
+message(STATUS "Using maya_DIR = ${maya_DIR} (use '-Dmaya_DIR=xxx' to override)")
 
 find_path(maya_INCLUDE_PATH NAMES "maya/MApiVersion.h" PATHS "${maya_DIR}/include" NO_DEFAULT_PATH)
-message(STATUS "detected maya include dir: ${maya_INCLUDE_PATH}")
 
-find_library(maya_LINK_LIB_FOUNDATION NAMES "Foundation" PATHS "${maya_DIR}/lib")
-find_library(maya_LINK_LIB_OPENMAYA   NAMES "OpenMaya"   PATHS "${maya_DIR}/lib")
-find_library(maya_LINK_LIB_OPENMAYAUI NAMES "OpenMayaUI" PATHS "${maya_DIR}/lib")
+set(MAYA_LIB_DIR "${maya_DIR}/lib")
+find_library(maya_LINK_LIB_FOUNDATION NAMES "Foundation" PATHS "${MAYA_LIB_DIR}")
+find_library(maya_LINK_LIB_OPENMAYA   NAMES "OpenMaya"   PATHS "${MAYA_LIB_DIR}")
+find_library(maya_LINK_LIB_OPENMAYAUI NAMES "OpenMayaUI" PATHS "${MAYA_LIB_DIR}")
 list(APPEND maya_LINK_LIBRARIES ${maya_LINK_LIB_FOUNDATION} ${maya_LINK_LIB_OPENMAYA} ${maya_LINK_LIB_OPENMAYAUI})
 
 
 ### plugin installation location
 
 if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-	set(CMAKE_INSTALL_PREFIX "${CMAKE_CURRENT_LIST_DIR}/../install" CACHE PATH "default install path" FORCE)
+	set(CMAKE_INSTALL_PREFIX "${CMAKE_SOURCE_DIR}/../install" CACHE PATH "default install path" FORCE)
 endif()
 message(STATUS "Installing into ${CMAKE_INSTALL_PREFIX}")
