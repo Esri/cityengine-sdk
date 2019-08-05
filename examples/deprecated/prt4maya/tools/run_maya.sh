758cd15dc346060b9509c1117c809ffca06ac13a
Simon Haegler <shaegler@esri.com>
2019-08-05 15:44:59 +0200

    deleting the deprecated "prt4maya" example
    for reference, the state of the "prt4maya" example before the redesign
    is available in the "v0.1-redesign" tag in the github.com/esri/serlio
    repo

--- examples/deprecated/prt4maya/tools/run_maya.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ROOT="$(dirname "$0")"/..
-
-export MAYA_PLUG_IN_PATH="${MAYA_PLUG_IN_PATH}:${ROOT}/install"
-export MAYA_SCRIPT_PATH="${MAYA_SCRIPT_PATH}:${ROOT}/install"
-#maya -d gdb
-maya
