98262381fa6aa7980c7d706f1dcfcfcebc209632
Simon Haegler <shaegler@esri.com>
2019-10-01 18:04:25 +0200

    Update PRT reference documentation to 2.1.5704

--- html/search/all_6c.js
+++ html/search/all_6c.js
@@ -18,7 +18,6 @@ var searchData=
   ['log_5fwarning',['LOG_WARNING',['../namespaceprt.html#ac35a3cef5502bd6490d1742bd427916aa475efbc5d40df41772c5a58032ef296e',1,'prt']]],
   ['logformatter',['LogFormatter',['../classprtx_1_1_log_formatter.html#accdb8984a304bb0613b8748971282ea9',1,'prtx::LogFormatter::LogFormatter(prt::LogLevel l)'],['../classprtx_1_1_log_formatter.html#a7842b2226837982224f6117eafa2edb3',1,'prtx::LogFormatter::LogFormatter(prt::LogLevel l, const std::string &amp;s)'],['../classprtx_1_1_log_formatter.html#aac8aa49d7fe5305003e8a2509083d18e',1,'prtx::LogFormatter::LogFormatter(prt::LogLevel l, const std::string &amp;s, const std::string &amp;p)'],['../classprtx_1_1_log_formatter.html#af3af1c3c2f80e2f7619f66c62a40f54c',1,'prtx::LogFormatter::LogFormatter(prt::LogLevel l, const std::exception &amp;e)'],['../classprtx_1_1_log_formatter.html#a8aa6b2002e2fb69b15c46b42dc18b334',1,'prtx::LogFormatter::LogFormatter(prt::LogLevel l, const std::string &amp;s, const std::exception &amp;e)']]],
   ['logformatter',['LogFormatter',['../classprtx_1_1_log_formatter.html',1,'prtx']]],
-  ['logformatterw',['LogFormatterW',['../classprtx_1_1_log_formatter_w.html',1,'prtx']]],
   ['logfwd',['LogFwd',['../classprtx_1_1_log_fwd.html',1,'prtx']]],
   ['logfwd_3c_20true_2c_20l_2c_20lt_20_3e',['LogFwd&lt; true, L, LT &gt;',['../classprtx_1_1_log_fwd_3_01true_00_01_l_00_01_l_t_01_4.html',1,'prtx']]],
   ['loghandler',['LogHandler',['../classprt_1_1_log_handler.html',1,'prt']]],
--- html/search/classes_6f.js
+++ html/search/classes_6f.js
@@ -4,6 +4,8 @@ var searchData=
   ['objectdestroyer',['ObjectDestroyer',['../structprtx_1_1_p_r_t_utils_1_1_object_destroyer.html',1,'prtx::PRTUtils']]],
   ['objectptr',['ObjectPtr',['../classprtx_1_1_p_r_t_utils_1_1_object_ptr.html',1,'prtx::PRTUtils']]],
   ['occlusionset',['OcclusionSet',['../classprt_1_1_occlusion_set.html',1,'prt']]],
+  ['opaqueptr',['OpaquePtr',['../classprtx_1_1_opaque_ptr.html',1,'prtx']]],
+  ['opaqueptr_3c_20logformatterimpl_20_3e',['OpaquePtr&lt; LogFormatterImpl &gt;',['../classprtx_1_1_opaque_ptr.html',1,'prtx']]],
   ['optionannotator',['OptionAnnotator',['../classprtx_1_1_encode_options_annotator_1_1_option_annotator.html',1,'prtx::EncodeOptionsAnnotator']]],
   ['optionstates',['OptionStates',['../classprtx_1_1_option_states.html',1,'prtx']]]
 ];
