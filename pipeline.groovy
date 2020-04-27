4328ac862f30fd69ba77885e9976adbe8ce02c38
Simon Haegler <shaegler@esri.com>
2020-04-27 10:34:25 +0200

    CE-7719: CI: switch to CE SDK 2.2 (CE 2020.0)

--- pipeline.groovy
+++ pipeline.groovy
@@ -65,9 +65,9 @@ Map taskGenSTLDEC() {
 
 def taskBuildPRT4CMD(cfg) {
 	final String appName = 'prt4cmd'
-	final List DEPS = [ PrtAppPipelineLibrary.Dependencies.CESDK ]
+	final List DEPS = [ PrtAppPipelineLibrary.Dependencies.CESDK20200 ]
 	final List defs = [
-		[ key: 'prt_DIR',               val: PrtAppPipelineLibrary.Dependencies.CESDK.p ],
+		[ key: 'prt_DIR',               val: PrtAppPipelineLibrary.Dependencies.CESDK20200.p ],
 		[ key: 'PRT4CMD_VERSION_MAJOR', val: 0 ],
 		[ key: 'PRT4CMD_VERSION_MINOR', val: 0 ],
 		[ key: 'PRT4CMD_VERSION_MICRO', val: env.BUILD_NUMBER ]
@@ -78,9 +78,9 @@ def taskBuildPRT4CMD(cfg) {
 
 def taskBuildSTLENC(cfg) {
 	final String appName = 'stlenc'
-	final List DEPS = [ PrtAppPipelineLibrary.Dependencies.CESDK ]
+	final List DEPS = [ PrtAppPipelineLibrary.Dependencies.CESDK20200 ]
 	List defs = [
-		[ key: 'prt_DIR',              val: PrtAppPipelineLibrary.Dependencies.CESDK.p ],
+		[ key: 'prt_DIR',              val: PrtAppPipelineLibrary.Dependencies.CESDK20200.p ],
 		[ key: 'STLENC_VERSION_MICRO', val: env.BUILD_NUMBER ]
 	]
 	papl.buildConfig(REPO, myBranch, "${SOURCES}/${appName}/src", BUILD_TARGET, cfg, DEPS, defs, REPO_CREDS)
@@ -89,9 +89,9 @@ def taskBuildSTLENC(cfg) {
 
 def taskBuildSTLDEC(cfg) {
 	final String appName = 'stldec'
-	final List DEPS = [ PrtAppPipelineLibrary.Dependencies.CESDK ]
+	final List DEPS = [ PrtAppPipelineLibrary.Dependencies.CESDK20200 ]
 	List defs = [
-		[ key: 'prt_DIR',              val: PrtAppPipelineLibrary.Dependencies.CESDK.p ],
+		[ key: 'prt_DIR',              val: PrtAppPipelineLibrary.Dependencies.CESDK20200.p ],
 		[ key: 'STLDEC_VERSION_MICRO', val: env.BUILD_NUMBER ]
 	]
 	papl.buildConfig(REPO, myBranch, "${SOURCES}/${appName}/src", BUILD_TARGET, cfg, DEPS, defs, REPO_CREDS)
